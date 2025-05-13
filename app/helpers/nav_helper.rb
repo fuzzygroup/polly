463d4fc388c118c1c7ebfac7edbe9f4a3331b1f7
Scott Johnson <fuzzygroup@gmail.com>
2025-05-12 23:33:22 -0400

    Rejigger the top bar functionality for the Nth time

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -10,8 +10,9 @@ module NavHelper
   NAVS = [
     { path: "root_path", name: "Home" },
     #{ path: "polls_path", name: "Polls" },    
-    { path: "availabilities_path", name: "My Availability" },
+    { path: "events_path", name: "Events" },
     { path: "projects_path", name: "Projects" },
+    { path: "availabilities_path", name: "My Availability" },
     #{ path: "teams_path", name: "Teams" },
     # { path: "vetting_questions_path", name: "Ask Vetting Questions" },
     # { path: "fliers_path", name: "Fliers" }
